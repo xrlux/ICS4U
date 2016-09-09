fd6c57b763c050524e5e75813198b23beb52d7b8
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-09 13:38:33 -0400

    Adding HelloWorld, Greetings and Greetings2

---
+++ Hello/src/Richard/HelloWorld.java
@@ -0,0 +1,12 @@
+package Richard;
+
+/**
+ * Displays a "Hello World!" message on the screen
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
