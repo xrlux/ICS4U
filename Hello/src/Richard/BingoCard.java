319f62e320244113c4273135d6af29d8d4bcdfcb
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-15 13:41:27 -0400

    BingoCard

---
+++ Hello/src/Richard/BingoCard.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class BingoCard {
+
+	/** 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-1s %4s %4s %4s %4s","B","I","N","G","O\n");
+		System.out.format("%-1s %3s %4s %4s %4s","13","18","45","60","64\n");
+		System.out.format("%-1s %3s %4s %4s %4s","10","25","31","55","70\n");
+		System.out.format("%-1s %4s %4s %3s %4s","3","27"," FREE","53","67\n");
+		System.out.format("%-1s %4s %4s %4s %4s","7","29","44","46","75\n");
+		System.out.format("%-1s %3s %4s %4s %4s","12","24","39","59","71\n");
+	}
+
+}
