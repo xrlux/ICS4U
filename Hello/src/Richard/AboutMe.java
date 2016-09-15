21f7aff9d85a6bf7f592c07ca2df1981cabefad4
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-15 13:24:57 -0400

    AboutMe

---
+++ Hello/src/Richard/AboutMe.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Richard Lu");
+		System.out.println("Ms.Kemp");
+		System.out.println("Runnymede");
+		
+		System.out.format("%-10s %8s %8s","","day 1","\n");
+		System.out.format("%-10s %8s %8s","periods","class","   start time\n");
+		System.out.format("%-10s %8s %8s","period 1","ENG3CU","   8:45-10:02\n");
+		System.out.format("%-10s %8s %8s","period 2","PPL3OM","   10:07-11:24\n");
+		System.out.format("%-10s %8s %8s","lunch","","   11:24-12:26\n");
+		System.out.format("%-10s %8s %8s","period 3","ICS3U1","   12:26-1:45\n");
+		System.out.format("%-10s %8s %8s","period 4","SBI3C1","   1:48-3:04\n");
+	}
+}
