5fe48fb60d8c5a774b5edf30bcb5b0d5251ba024
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-19 13:09:31 -0400

    BingoRules

---
+++ Hello/src/Richard/BingoRules.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class BingoRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-1s %4s %4s %4s %4s","Bingo Card","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","1.","The caller randomly pulls a numbered bingo ball.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","2.","The number is placed on the bingo board and called out.","","","\n");
+		System.out.format("%-1s %4s %4s %3s %4s","3.","Players look for the called number on their bingo card.","","","\n");
+		System.out.format("%-1s %4s %4s %4s %4s","4.","If the number is located, it is marked off.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","5.","Steps 1 to 4 are repeated until a player matches the BINGO pattern.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","6.","The winning player yells BINGO.","","","\n");
+	}
+
+}
