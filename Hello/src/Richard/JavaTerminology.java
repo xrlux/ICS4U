395ca5237868b4b4ce04568f18581f233d746f5c
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-19 13:33:52 -0400

    Java Terminology

---
+++ Hello/src/Richard/JavaTerminology.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class JavaTerminology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-1s %4s %4s %4s %4s","Execute:","To run a program.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","Comment:","Text that provides information to the reader of the program code.","","","\n");
+		System.out.format("%-1s %4s %4s %3s %4s","","","","","\n");
+		System.out.format("%-1s %4s %4s %4s %4s","Algorithm:","A set of steps that outline how to solve a problem.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","Source code:","The code typed by a programmer.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","String:","A set of characters.","","","\n");
+	}
+
+}
