62b12efe3bd377541e8ff0f7a896e78d95f3d401
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-19 13:25:36 -0400

    Tic Tac Toe rules

---
+++ Hello/src/Richard/TicTacToeRules.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class TicTacToeRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.format("%-1s %4s %4s %4s %4s","Tic Tac Toe Rules","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","Tic Tac Toe is a game for 2 players.","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","1.","A grid of 9 squares is drawn.","","","\n");
+		System.out.format("%-1s %4s %4s %3s %4s","2.","The first player draws an X in an empty square.","","","\n");
+		System.out.format("%-1s %4s %4s %4s %4s","3.","The second player draws an O in an empty square.","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","4.","Steps 2 and 3 are repeated until a Tic Tac Pattern is created:"
+				+ "","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","	Patterns can be diagonal, horizontal, or vertical","","","","\n");
+		System.out.format("%-1s %3s %4s %4s %4s","5.","A winning pattern is completed by drawing a line through the pattern.","","","\n");
+
+	}
+
+}
