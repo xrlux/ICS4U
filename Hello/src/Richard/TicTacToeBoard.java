0eedda9851bd16da76da8bd165a67fc311845c78
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-09-19 13:16:21 -0400

    TicTacToe board

---
+++ Hello/src/Richard/TicTacToeBoard.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package Richard;
+
+/**
+ * @author 323828715
+ *
+ */
+public class TicTacToeBoard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s","---------------------\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|","X","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s","---------------------\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n");
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+		 System.out.format("%6s%3s%8s","|"," ","|\n"); 
+	}
+
+}
