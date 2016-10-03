bbd6ee5fa0cdcbc7744cbc997344d0e8b8405af0
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-10-03 13:27:39 -0400

    Quadratics

---
+++ Unit2/src/Lu/Quadratics.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Lu;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/**
+ * @author 323828715
+ *
+ */
+public class Quadratics {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat ("0.00");
+		int a;
+		int b;
+		int c;
+		
+		System.out.println("Please enter 3 numbers:");
+		a = scan.nextInt() ;
+		b = scan.nextInt() ;
+		c = scan.nextInt() ;
+		
+		System.out.println(df.format((-b + Math.sqrt((Math.pow(b, 2)-4*a*c)))/(2*a)));
+		System.out.println(df.format((-b - Math.sqrt((Math.pow(b, 2)-4*a*c)))/(2*a)));
+
+	}
+
+}
