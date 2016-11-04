5f5f28ac701c56627100c5752c35791cb8131a8a
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-11-04 12:55:38 -0400

    aplusb

---
+++ PEG/src/aplusb.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class aplusb {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner (System.in);
+		int a =scan.nextInt();
+		int b = scan.nextInt();
+		System.out.print(a+b);
+
+	}
+
+}
