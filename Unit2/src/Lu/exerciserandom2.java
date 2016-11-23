327637f0b1d6252cf384c7baa32ad016ddf7ad36
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2016-11-23 13:11:27 -0500

    excercise2 with loop

--- Unit2/src/Lu/exerciserandom2.java
+++ Unit2/src/Lu/exerciserandom2.java
@@ -2,7 +2,7 @@
  * 
  */
 package Lu;
-
+import java.util.Scanner;
 /**
  * @author 323828715
  *
@@ -14,7 +14,9 @@ public class exerciserandom2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner scan=new Scanner(System.in);
+		int play1;
+		do{
 		int dice1= (int)(Math.random()*11)+2;
 		int dice2= (int)(Math.random()*11)+2;
 		
@@ -27,6 +29,9 @@ public class exerciserandom2 {
 		else if (dice1==dice2)
 			System.out.println("It's a tie!!!");
 
+		System.out.println("Would you like to play again, press 1");
+		play1=scan.nextInt();
+		}while(play1==1);
 	}
 
 }
