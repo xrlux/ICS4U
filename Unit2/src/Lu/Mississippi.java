8b427f3e49264a0ea183a58520d43641812a9233
323828715 <323828715@C-4BDQKS1.tdsb.on.ca>
2017-01-13 12:51:10 -0500

    Mississippi

---
+++ Unit2/src/Lu/Mississippi.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package Lu;
+
+/**
+ * @author 323828715
+ *
+ */
+public class Mississippi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		m();
+		i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();
+	}
+
+			public static void m(){
+		
+		System.out.println("M          M");
+		System.out.println("M M      M M");
+		System.out.println("M   M  M   M");
+		System.out.println("M    M     M");
+		System.out.println("M          M");
+		System.out.println("M          M"); 		
+}
+			public static void i(){	
+		System.out.println("IIIIIIIIIIII");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("IIIIIIIIIIII"); 			
+			}
+			public static void s(){	
+		System.out.println("SSSSSSSSSSSS");
+		System.out.println("SSSS        ");
+		System.out.println("  SSSS      ");
+		System.out.println("     SSSS   ");
+		System.out.println("        SSS ");
+		System.out.println("SSSSSSSSSSSS"); 	
+			}
+			public static void p(){	
+		System.out.println("PPPPPPPPPPPP");
+		System.out.println("PPP      PPP");
+		System.out.println("PPP      PPP");
+		System.out.println("PPPPPPPPPPPP");
+		System.out.println("PPP ");
+		System.out.println("PPP ");	
+			
+}
+			}
+			
+			
